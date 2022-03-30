041e26cb58fa7f07237a9a659bf1d8670899bf6b
Thomas Haudiquer <thomashaudiquer@gmail.comm>
2022-03-30 16:09:53 +0100

    commit

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
+let postsArray = []
 
- fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+function renderPosts() {
+    let html = ""
+    for (let post of postsArray) {
+        html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `
+    }
+    document.getElementById("blog-list").innerHTML = html
+    
+}
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
-        const postsArr = data.slice(0, 5)
-        let html = ""
-        for (let post of postsArr) {
-            html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `
-        }
-        document.getElementById("blog-list").innerHTML = html
+        postsArray = data.slice(0, 5)
+        renderPosts()
     })
-
 
 document.getElementById("new-post").addEventListener("submit", function(e) {
     e.preventDefault()
-    const postTitle = document.getElementById("post-title").value;
-    const postBody = document.getElementById("post-body").value;
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
     const data = {
         title: postTitle,
         body: postBody
     }
+    
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(post => {
+            postsArray.unshift(post)
+            renderPosts()
+            document.getElementById("new-post").reset()
+        })
 })
-
-//test
-
